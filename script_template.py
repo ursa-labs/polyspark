8dfb132608175b942bd7a88483258ba8e111b81e
Ian Cook <ianmcook@gmail.com>
2021-01-26 17:40:21 -0500

    Remove unnecessary __name__ handling from example

--- script_template.py
+++ script_template.py
@@ -1,9 +1,8 @@
 from pyspark.sql import SparkSession
 # add additional imports here if needed
 
-if __name__ == "__main__":
-    spark = SparkSession.builder.getOrCreate()
-
-    # add your code here
-
-    spark.stop()
+spark = SparkSession.builder.getOrCreate()
+#
+# add your code here
+#
+spark.stop()
